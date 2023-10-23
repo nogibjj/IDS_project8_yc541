915e5740dff58d4b7e1e4bacb21448e436ce7d2a
Ashley Cao <52900646+yuanc0000@users.noreply.github.com>
2023-10-23 18:37:25 +0000

    update rust code and csv

--- rust_spotify_analysis/src/main.rs
+++ rust_spotify_analysis/src/main.rs
@@ -1,35 +1,36 @@
-extern crate csv;
-extern crate polars;
-extern crate plotters;
-
 use polars::prelude::*;
-use plotters::prelude::*;
+use std::error::Error as StdError;
+use std::result::Result as StdResult;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Load the dataset
-    let df = CsvReader::from_path("spotify-2023.csv")?.infer_schema(None).has_header(true).finish()?;
+fn main() -> StdResult<(), Box<dyn StdError>> {
+    let file_path = "spotify-2023.csv";
 
-    // Print the first 5 rows
-    println!("{:?}", df.head(Some(5)));
+    let mut df = CsvReader::from_path(file_path)?
+        .infer_schema(None)
+        .has_header(true)
+        .finish()?;
 
-    // Generate summary statistics for numeric columns
-    // NOTE: This is simplified. For detailed statistics, you may need custom functions.
-    println!("{:?}", df.describe());
+    // 打印数据集的前十行
+    println!("=== Dataset Overview ===");
+    println!("{:?}", df.head(Some(10)));
 
-    // Handle non-numeric values in 'streams' column and convert it to integer type
-    // NOTE: You might need to preprocess your CSV or implement more advanced parsing.
-    let df = df.filter(df.column("streams").is_ok()).unwrap();
-    let top_10_songs = df.sort("streams", false).unwrap().head(Some(10)).unwrap();
+    // 对于streams列，先检查它是否存在，然后尝试转换和过滤操作
+    if let Ok(streams_column) = df.column("streams") {
+        let streams_column = streams_column.clone();
+        let boolean_array = streams_column.is_not_null();
+        df.filter(&boolean_array)?;
+        if let Ok(streams_as_uint) = streams_column.cast::<UInt32Type>() {
+            df.with_column(streams_as_uint)?;
+        }
+    }
 
-    println!("{:?}", top_10_songs);
+    // 获取前10首歌曲
+    let sorted_df = df.sort("streams", false)?;
+    let top_10_songs = sorted_df.head(Some(10));
 
-    // Data visualization (simplified)
-    // Implementing a bar chart using plotters. Detailed chart settings will be more verbose than seaborn.
-
-    // NOTE: This is a very high-level overview. The actual Rust code for plotting may be more involved.
-    // You can refer to the `plotters` documentation for more detailed examples.
-
-    // ... plotting code ...
+    
+    println!("=== Top 10 Songs by Streams ===");
+    println!("{:?}", top_10_songs.select(vec!["track_name", "artist(s)_name", "streams"]));
 
     Ok(())
 }
